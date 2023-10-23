869c9d9060bb6d06fe46bff1d7f5d86afe897b6b
rndnc <rr.m.amor94@gmail.com>
2023-10-23 22:22:17 +0900

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// Route::post('/apiSales','App\Http\Controllers\SaleController@apiSales');
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware(['middleware' => 'api'])->group(function () {
+    // API 商品の在庫数を減らす処理
+    Route::post('/apiSales',[SaleController::class, 'apiSales']);
+    // API 商品の在庫数を増やす処理
+//     Route::post('/apiAdd',[SalesController::class, 'apiAdd']);
 });
